58dfa4840539767713d9d7c0b0ef9aeec51006ee
comrade-T <communism.app@gmail.com>
2024-06-28 04:51:24 +0700

    exp(pad): figure out how Raylib Gamepad API

--- src/main.zig
+++ src/main.zig
@@ -5,7 +5,7 @@ pub fn main() anyerror!void {
     const screenWidth = 800;
     const screenHeight = 450;
 
-    r.initWindow(screenWidth, screenHeight, "Communism");
+    r.initWindow(screenWidth, screenHeight, "App");
     defer r.closeWindow();
 
     r.setTargetFPS(60);
@@ -20,5 +20,17 @@ pub fn main() anyerror!void {
 
         r.clearBackground(r.Color.blank);
         r.drawText("Congrats! You created your first window!", 190, 200, 20, r.Color.sky_blue);
+
+        const gamepad = 1;
+
+        std.debug.print("is X: {any}\n", .{r.isGamepadButtonDown(gamepad, r.GamepadButton.gamepad_button_right_face_left)});
+        std.debug.print("is A: {any}\n", .{r.isGamepadButtonDown(gamepad, r.GamepadButton.gamepad_button_right_face_down)});
+
+        var i: i32 = 0;
+        const axis_count = r.getGamepadAxisCount(gamepad);
+        while (i < axis_count) {
+            std.debug.print("axis {d} value: {d}\n", .{ i, r.getGamepadAxisMovement(gamepad, i) });
+            i += 1;
+        }
     }
 }
