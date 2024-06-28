f50d6c86411394d20f430cc5a78adf8fb7ee6573
comrade-T <communism.app@gmail.com>
2024-06-28 19:18:10 +0700

    wip(hot): add hot reload skeleton w/ blue ball

---
+++ src/game.zig
@@ -0,0 +1,65 @@
+const std = @import("std");
+const r = @cImport({
+    @cInclude("raylib.h");
+});
+
+//////////////////////////////////////////////////////////////////////////////////////////////
+
+const screen_w = 800;
+const screen_h = 450;
+
+const GameState = struct {
+    allocator: std.mem.Allocator,
+    time: f32 = 0,
+    radius: f32 = 0,
+};
+
+//////////////////////////////////////////////////////////////////////////////////////////////
+
+export fn gameInit(allocator_ptr: *anyopaque) *anyopaque {
+    const allocator: *std.mem.Allocator = @ptrCast(@alignCast(allocator_ptr));
+    const gs = allocator.create(GameState) catch @panic("Out of memory.");
+
+    gs.* = GameState{
+        .allocator = allocator.*,
+        .radius = readRadiusConfig(allocator.*),
+    };
+
+    return gs;
+}
+
+export fn gameReload(game_state_ptr: *anyopaque) void {
+    var gs: *GameState = @ptrCast(@alignCast(game_state_ptr));
+    gs.radius = readRadiusConfig(gs.allocator);
+}
+
+export fn gameTick(game_state_ptr: *anyopaque) void {
+    var gs: *GameState = @ptrCast(@alignCast(game_state_ptr));
+    gs.time += r.GetFrameTime();
+}
+
+export fn gameDraw(game_state_ptr: *anyopaque) void {
+    const gs: *GameState = @ptrCast(@alignCast(game_state_ptr));
+    r.ClearBackground(r.BLANK);
+
+    var buf: [256]u8 = undefined;
+    const slice = std.fmt.bufPrintZ(&buf, "radius: {d:.02}, time: {d:.02}", .{ gs.radius, gs.time }) catch "error";
+    r.DrawText(slice, 10, 10, 20, r.RAYWHITE);
+
+    const circle_x: f32 = @mod(gs.time * 240.0, screen_w + gs.radius * 2) - gs.radius;
+    r.DrawCircleV(.{ .x = circle_x, .y = screen_h - gs.radius - 40 }, gs.radius, r.BLUE);
+}
+
+fn readRadiusConfig(allocator: std.mem.Allocator) f32 {
+    const default_value: f32 = 40.0;
+    const config_filepath = "config/radius.txt";
+    const config_data = std.fs.cwd().readFileAlloc(allocator, config_filepath, 1024 * 1024) catch {
+        std.debug.print("Failed to read {s}\n", .{config_filepath});
+        return default_value;
+    };
+    defer allocator.free(config_data);
+    return std.fmt.parseFloat(f32, config_data[0 .. config_data.len - 1]) catch {
+        std.debug.print("Failed to parse {s} in {s}\n", .{ config_data, config_filepath });
+        return default_value;
+    };
+}
--- src/main.zig
+++ src/main.zig
@@ -7,27 +7,80 @@ const r = @cImport({
 const gp_state = @import("gamepad/state.zig");
 const gp_view = @import("gamepad/view.zig");
 
-pub fn main() !void {
-    const screen_w = 800;
-    const screen_h = 450;
+//////////////////////////////////////////////////////////////////////////////////////////////
 
+const GameStatePtr = *anyopaque;
+
+var gameInit: *const fn (*anyopaque) callconv(.C) GameStatePtr = undefined;
+var gameReload: *const fn (GameStatePtr) callconv(.C) void = undefined;
+var gameTick: *const fn (GameStatePtr) callconv(.C) void = undefined;
+var gameDraw: *const fn (GameStatePtr) callconv(.C) void = undefined;
+
+//////////////////////////////////////////////////////////////////////////////////////////////
+
+const screen_w = 800;
+const screen_h = 450;
+const failed_to_load_msg = "Faild to load game.dll";
+
+//////////////////////////////////////////////////////////////////////////////////////////////
+
+pub fn main() !void {
     r.InitWindow(screen_w, screen_h, "App");
     defer r.CloseWindow();
+
+    loadGameDll() catch @panic(failed_to_load_msg);
 
     r.SetTargetFPS(60);
     r.SetExitKey(r.KEY_NULL);
     r.SetConfigFlags(r.FLAG_WINDOW_TRANSPARENT);
 
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer _ = gpa.deinit();
+    const allocator = gpa.allocator();
+
+    const game_state = gameInit(allocator.ptr);
+
     while (!r.WindowShouldClose()) {
+        // if (checker.should_reload()) {
+        if (r.IsKeyPressed(r.KEY_F5)) {
+            unloadGameDll() catch unreachable;
+            loadGameDll() catch @panic(failed_to_load_msg);
+            gameReload(game_state);
+            // checker.set_should_reload_on_next_loop(false);
+        }
+
+        gameTick(game_state);
         r.BeginDrawing();
-        defer r.EndDrawing();
+        gameDraw(game_state);
+        r.EndDrawing();
 
-        r.ClearBackground(r.BLANK);
-        r.DrawText("Congrats! You created your first window!", 190, 50, 20, r.SKYBLUE);
+        // const gamepad = 1;
+        // const state = gp_state.getGamepadState(gamepad);
+        // gp_view.drawGamepadState(state);
+    }
+}
 
-        const gamepad = 1;
-        const state = gp_state.getGamepadState(gamepad);
+//////////////////////////////////////////////////////////////////////////////////////////////
 
-        gp_view.drawGamepadState(state);
+var game_dyn_lib: ?std.DynLib = null;
+fn loadGameDll() !void {
+    if (game_dyn_lib != null) return error.AlreadyLoaded;
+    var dyn_lib = std.DynLib.open("zig-out/lib/libgame.so") catch {
+        return error.OpenFail;
+    };
+    game_dyn_lib = dyn_lib;
+    gameInit = dyn_lib.lookup(@TypeOf(gameInit), "gameInit") orelse return error.LookupFail;
+    gameReload = dyn_lib.lookup(@TypeOf(gameReload), "gameReload") orelse return error.LookupFail;
+    gameTick = dyn_lib.lookup(@TypeOf(gameTick), "gameTick") orelse return error.LookupFail;
+    gameDraw = dyn_lib.lookup(@TypeOf(gameDraw), "gameDraw") orelse return error.LookupFail;
+    std.debug.print("Loaded game.dll\n", .{});
+}
+
+fn unloadGameDll() !void {
+    if (game_dyn_lib) |*dyn_lib| {
+        dyn_lib.close();
+        game_dyn_lib = null;
+    } else {
+        return error.AlreadyUnloaded;
     }
 }
