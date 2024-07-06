afdd9e410d2bfbbe98f6b15f27c9d40b832f4418
comrade-T <communism.app@gmail.com>
2024-07-07 05:59:47 +0700

    wip(buf): add load_from_string() and load()

--- src/buffer/buffer.zig
+++ src/buffer/buffer.zig
@@ -11,7 +11,7 @@ const eqStr = std.testing.expectEqualStrings;
 
 //////////////////////////////////////////////////////////////////////////////////////////////
 
-const Buffer = struct {
+pub const Buffer = struct {
     external_allocator: Allocator,
     arena: std.heap.ArenaAllocator,
     a: Allocator,
@@ -33,9 +33,49 @@ const Buffer = struct {
         self.external_allocator.destroy(self);
     }
 
-    pub fn load_from_string(self: *const Buffer, s: []const u8) !Root {
+    fn load_from_string(self: *const Buffer, s: []const u8) !Root {
         var stream = std.io.fixedBufferStream(s);
         return self.load(stream.reader(), s.len);
+    }
+
+    fn load(self: *const Buffer, reader: anytype, size: usize) !Root {
+        const buf = try self.a.alloc(u8, size);
+
+        const read_size = try reader.read(buf);
+        if (read_size != size) return error.BufferUnderrun;
+
+        const final_read = try reader.read(buf);
+        if (final_read != 0) @panic("unexpected data in final read");
+
+        const leaves = try _create_leaves(self.a, buf);
+        return Node.merge_in_place(self.a, leaves);
+    }
+
+    fn _create_leaves(a: std.mem.Allocator, buf: []const u8) ![]Node {
+        const eol = '\n';
+
+        var leaf_count: usize = 1;
+        for (0..buf.len) |i| {
+            if (buf[i] == eol) leaf_count += 1;
+        }
+
+        var leaves = try a.alloc(Node, leaf_count);
+        var cur_leaf: usize = 0;
+        var b: usize = 0;
+        for (0..buf.len) |i| {
+            if (buf[i] == eol) {
+                const line = buf[b..i];
+                leaves[cur_leaf] = .{ .leaf = .{ .buf = line, .bol = true, .eol = true } };
+                cur_leaf += 1;
+                b = i + 1;
+            }
+        }
+
+        const rest = buf[b..];
+        leaves[cur_leaf] = .{ .leaf = .{ .buf = rest, .bol = true, .eol = false } };
+
+        if (leaves.len != cur_leaf + 1) return error.Unexpected;
+        return leaves;
     }
 };
 
@@ -74,7 +114,7 @@ const Node = union(enum) {
     }
 };
 
-pub const Branch = struct {
+const Branch = struct {
     left: *const Node,
     right: *const Node,
     weights: Weights,
@@ -86,7 +126,7 @@ const empty_bol_leaf: Node = .{ .leaf = .{ .buf = "", .bol = true, .eol = false 
 const empty_eol_leaf: Node = .{ .leaf = .{ .buf = "", .bol = false, .eol = true } };
 const empty_line_leaf: Node = .{ .leaf = .{ .buf = "", .bol = true, .eol = true } };
 
-pub const Leaf = struct {
+const Leaf = struct {
     buf: []const u8,
     bol: bool = true,
     eol: bool = true,
@@ -114,7 +154,7 @@ pub const Leaf = struct {
     }
 };
 
-pub const Weights = struct {
+const Weights = struct {
     bols: u32 = 0,
     eols: u32 = 0,
     len: u32 = 0,
@@ -130,13 +170,67 @@ pub const Weights = struct {
 
 //////////////////////////////////////////////////////////////////////////////////////////////
 
-test Buffer {
+test "Buffer.create() & Buffer.deinit()" {
     const empty_buffer = try Buffer.create(std.testing.allocator, std.testing.allocator);
     defer empty_buffer.deinit();
 
     try eqDeep(Weights{ .bols = 0, .eols = 0, .len = 0, .depth = 2 }, empty_buffer.root.weights_sum());
     try eqDeep(Weights{ .bols = 0, .eols = 0, .len = 0, .depth = 1 }, empty_buffer.root.node.left.weights_sum());
     try eqDeep(Weights{ .bols = 0, .eols = 0, .len = 0, .depth = 1 }, empty_buffer.root.node.right.weights_sum());
+}
+
+test "Buffer.load_from_string()" {
+    const buffer = try Buffer.create(std.testing.allocator, std.testing.allocator);
+    defer buffer.deinit();
+
+    {
+        const root = try buffer.load_from_string("ayaya");
+        try eqDeep(Weights{ .bols = 1, .eols = 0, .len = 5, .depth = 1 }, root.weights_sum());
+        try eqStr("ayaya", root.leaf.buf);
+    }
+
+    {
+        const root = try buffer.load_from_string("hello\nworld");
+        try eqDeep(Weights{ .bols = 2, .eols = 1, .len = 11, .depth = 2 }, root.weights_sum());
+        try eqDeep(Weights{ .bols = 1, .eols = 1, .len = 6, .depth = 1 }, root.node.left.weights_sum());
+        try eqDeep(Weights{ .bols = 1, .eols = 0, .len = 5, .depth = 1 }, root.node.right.weights_sum());
+        try eqStr("hello", root.node.left.leaf.buf);
+        try eqStr("world", root.node.right.leaf.buf);
+    }
+}
+
+test "Buffer._create_leaves()" {
+    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    defer arena.deinit();
+    const a = arena.allocator();
+
+    {
+        const str = "hello";
+        const expected = [_]Node{
+            Node{ .leaf = Leaf{ .buf = "hello", .bol = true, .eol = false } },
+        };
+        try eqDeep(&expected, try Buffer._create_leaves(a, str));
+    }
+
+    {
+        const str = "hello\nworld";
+        const expected = [_]Node{
+            Node{ .leaf = Leaf{ .buf = "hello", .bol = true, .eol = true } },
+            Node{ .leaf = Leaf{ .buf = "world", .bol = true, .eol = false } },
+        };
+        try eqDeep(&expected, try Buffer._create_leaves(a, str));
+    }
+
+    {
+        const str = "hello\nfrom\nthe\nother side";
+        const expected = [_]Node{
+            Node{ .leaf = Leaf{ .buf = "hello", .bol = true, .eol = true } },
+            Node{ .leaf = Leaf{ .buf = "from", .bol = true, .eol = true } },
+            Node{ .leaf = Leaf{ .buf = "the", .bol = true, .eol = true } },
+            Node{ .leaf = Leaf{ .buf = "other side", .bol = true, .eol = false } },
+        };
+        try eqDeep(&expected, try Buffer._create_leaves(a, str));
+    }
 }
 
 test "Node.new()" {
