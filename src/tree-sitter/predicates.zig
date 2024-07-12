85c76341c3c49a5ec287b6e8c834383cf5603e0f
comrade-T <communism.app@gmail.com>
2024-07-12 16:13:23 +0700

    wip(ts): store predicates inside patterns field

--- src/tree-sitter/predicates.zig
+++ src/tree-sitter/predicates.zig
@@ -6,7 +6,6 @@ const b = @import("bindings.zig");
 const Query = b.Query;
 const PredicateStep = b.Query.PredicateStep;
 const Allocator = std.mem.Allocator;
-const StringList = std.ArrayList([]const u8);
 const eql = std.mem.eql;
 
 //////////////////////////////////////////////////////////////////////////////////////////////
@@ -15,74 +14,7 @@ pub const PredicatesFilter = struct {
     external_allocator: std.mem.Allocator,
     arena: std.heap.ArenaAllocator,
     a: std.mem.Allocator,
-    predicates: std.ArrayList(Predicate),
-
-    const PredicateError = error{ Unsupported, InvalidAmountOfSteps, InvalidArgument, OutOfMemory };
-    const Predicate = union(enum) {
-        eq: struct {
-            capture: []const u8,
-            target: []const u8,
-        },
-        any_of: struct {
-            capture: []const u8,
-            targets: StringList,
-        },
-    };
-
-    fn createPredicate(a: Allocator, query: *const Query, steps: []const PredicateStep) PredicateError!Predicate {
-        const name = query.getStringValueForId(@as(u32, @intCast(steps[0].value_id)));
-
-        if (steps[steps.len - 1].type != .done) {
-            std.log.err("Last step of this predicate {s} isn't .done.", .{name});
-            return PredicateError.InvalidArgument;
-        }
-
-        if (eql(u8, name, "eq?")) {
-            if (steps.len != 4) return PredicateError.InvalidAmountOfSteps;
-            if (steps[1].type != .capture) {
-                std.log.err("First argument of #eq? predicate must be type .capture, got {any}", .{steps[1].type});
-                return PredicateError.InvalidArgument;
-            }
-            if (steps[2].type != .string) {
-                std.log.err("Second argument of #eq? predicate must be type .string, got {any}", .{steps[2].type});
-                return PredicateError.InvalidArgument;
-            }
-
-            return Predicate{
-                .eq = .{
-                    .capture = query.getCaptureNameForId(@as(u32, @intCast(steps[1].value_id))),
-                    .target = query.getStringValueForId(@as(u32, @intCast(steps[2].value_id))),
-                },
-            };
-        }
-
-        if (eql(u8, name, "any-of?")) {
-            if (steps.len < 4) return PredicateError.InvalidAmountOfSteps;
-            if (steps[1].type != .capture) {
-                std.log.err("First argument of #eq? predicate must be type .capture, got {any}", .{steps[1].type});
-                return PredicateError.InvalidArgument;
-            }
-
-            var target_list = StringList.init(a);
-            errdefer target_list.deinit();
-            for (2..steps.len - 1) |i| {
-                if (steps[i].type != .string) {
-                    std.log.err("Arguments second and beyond of #any-of? predicate must be type .string, got {any}", .{steps[i].type});
-                    return PredicateError.InvalidArgument;
-                }
-                try target_list.append(query.getStringValueForId(steps[i].value_id));
-            }
-
-            return Predicate{
-                .any_of = .{
-                    .capture = query.getCaptureNameForId(@as(u32, @intCast(steps[1].value_id))),
-                    .targets = target_list,
-                },
-            };
-        }
-
-        return PredicateError.Unsupported;
-    }
+    patterns: [][]Predicate,
 
     pub fn init(external_allocator: Allocator, query: *const Query) !*@This() {
         var self = try external_allocator.create(@This());
@@ -91,19 +23,29 @@ pub const PredicatesFilter = struct {
             .external_allocator = external_allocator,
             .arena = std.heap.ArenaAllocator.init(external_allocator),
             .a = self.arena.allocator(),
-            .predicates = std.ArrayList(Predicate).init(self.a),
+            .patterns = undefined,
         };
 
+        var patterns = std.ArrayList([]Predicate).init(self.a);
+        errdefer patterns.deinit();
         for (0..query.getPatternCount()) |pattern_index| {
             const steps = query.getPredicatesForPattern(@as(u32, @intCast(pattern_index)));
+            var predicates = std.ArrayList(Predicate).init(self.a);
+            errdefer predicates.deinit();
 
-            if (steps.len == 0) continue;
-            if (steps[0].type != .string) continue;
+            var start: usize = 0;
+            for (steps, 0..) |step, i| {
+                if (step.type == .done) {
+                    const predicate = try Predicate.create(self.a, query, steps[start .. i + 1]);
+                    try predicates.append(predicate);
+                    start = i + 1;
+                }
+            }
 
-            const predicate = try createPredicate(self.a, query, steps);
-            try self.predicates.append(predicate);
+            try patterns.append(try predicates.toOwnedSlice());
         }
 
+        self.*.patterns = try patterns.toOwnedSlice();
         return self;
     }
 
@@ -111,4 +53,94 @@ pub const PredicatesFilter = struct {
         self.arena.deinit();
         self.external_allocator.destroy(self);
     }
+
+    fn isValid(_: *@This(), source: []const u8, match: Query.Match) bool {
+        for (match.captures(), 0..) |cap, i| {
+            const contents = source[cap.node.getStartByte()..cap.node.getEndByte()];
+            std.debug.print("i: {d}, contents: {s}\n", .{ i, contents });
+        }
+        return true;
+    }
+
+    pub fn nextMatch(self: *@This(), source: []const u8, cursor: *Query.Cursor) ?Query.Match {
+        while (true) {
+            const match = cursor.nextMatch() orelse return null;
+            if (self.isValid(source, match)) return match;
+        }
+    }
+
+    /////////////////////////////
+
+    const PredicateError = error{ InvalidAmountOfSteps, InvalidArgument, OutOfMemory };
+    const Predicate = union(enum) {
+        eq: struct {
+            capture: []const u8,
+            target: []const u8,
+        },
+        any_of: struct {
+            capture: []const u8,
+            targets: [][]const u8,
+        },
+        unsupported: enum { unsupported },
+
+        fn create(a: Allocator, query: *const Query, steps: []const PredicateStep) PredicateError!Predicate {
+            const name = query.getStringValueForId(@as(u32, @intCast(steps[0].value_id)));
+
+            if (steps[steps.len - 1].type != .done) {
+                std.log.err("Last step of this predicate {s} isn't .done.", .{name});
+                return PredicateError.InvalidArgument;
+            }
+
+            if (eql(u8, name, "eq?")) {
+                if (steps.len != 4) {
+                    std.debug.print("Expected steps.len == 4, got {d}\n", .{steps.len});
+                    return PredicateError.InvalidAmountOfSteps;
+                }
+
+                if (steps[1].type != .capture) {
+                    std.log.err("First argument of #eq? predicate must be type .capture, got {any}", .{steps[1].type});
+                    return PredicateError.InvalidArgument;
+                }
+                if (steps[2].type != .string) {
+                    std.log.err("Second argument of #eq? predicate must be type .string, got {any}", .{steps[2].type});
+                    return PredicateError.InvalidArgument;
+                }
+
+                return Predicate{
+                    .eq = .{
+                        .capture = query.getCaptureNameForId(@as(u32, @intCast(steps[1].value_id))),
+                        .target = query.getStringValueForId(@as(u32, @intCast(steps[2].value_id))),
+                    },
+                };
+            }
+
+            if (eql(u8, name, "any-of?")) {
+                if (steps.len < 4) return PredicateError.InvalidAmountOfSteps;
+
+                if (steps[1].type != .capture) {
+                    std.log.err("First argument of #eq? predicate must be type .capture, got {any}", .{steps[1].type});
+                    return PredicateError.InvalidArgument;
+                }
+
+                var targets = std.ArrayList([]const u8).init(a);
+                errdefer targets.deinit();
+                for (2..steps.len - 1) |i| {
+                    if (steps[i].type != .string) {
+                        std.log.err("Arguments second and beyond of #any-of? predicate must be type .string, got {any}", .{steps[i].type});
+                        return PredicateError.InvalidArgument;
+                    }
+                    try targets.append(query.getStringValueForId(steps[i].value_id));
+                }
+
+                return Predicate{
+                    .any_of = .{
+                        .capture = query.getCaptureNameForId(@as(u32, @intCast(steps[1].value_id))),
+                        .targets = try targets.toOwnedSlice(),
+                    },
+                };
+            }
+
+            return Predicate{ .unsupported = .unsupported };
+        }
+    };
 };
--- src/tree-sitter/ts.zig
+++ src/tree-sitter/ts.zig
@@ -44,15 +44,22 @@ const PredicatesFilter = @import("predicates.zig").PredicatesFilter;
 const eq = std.testing.expectEqual;
 const eqStr = std.testing.expectEqualStrings;
 
-fn getTreeForTesting(source: []const u8, patterns: []const u8) !struct { *b.Tree, *b.Query } {
+fn getTreeForTesting(source: []const u8, patterns: []const u8) !struct { *b.Tree, *b.Query, *b.Query.Cursor } {
     const ziglang = try b.Language.get("zig");
+
     var parser = try b.Parser.create();
     defer parser.destroy();
     try parser.setLanguage(ziglang);
-    return .{ try parser.parseString(null, source), try b.Query.create(ziglang, patterns) };
+
+    const tree = try parser.parseString(null, source);
+    const query = try b.Query.create(ziglang, patterns);
+    const cursor = try b.Query.Cursor.create();
+    cursor.execute(query, tree.getRootNode());
+
+    return .{ tree, query, cursor };
 }
 
-test "PredicatesFilter" {
+test PredicatesFilter {
     const a = std.testing.allocator;
     const source =
         \\const std = @import("std");
@@ -64,20 +71,37 @@ test "PredicatesFilter" {
         \\
         \\((BUILTINIDENTIFIER) @include
         \\  (#any-of? @include "@import" "@cImport"))
+        \\
+        \\((IDENTIFIER) @contrived-example
+        \\  (#eq? @contrived-example "@contrived")
+        \\  (#contrived-predicate? @contrived-example "contrived-argument"))
     ;
 
-    const tree, const query = try getTreeForTesting(source, patterns);
+    const tree, const query, const cursor = try getTreeForTesting(source, patterns);
     defer tree.destroy();
     defer query.destroy();
+    defer cursor.destroy();
 
     var filter = try PredicatesFilter.init(a, query);
     defer filter.deinit();
 
-    try eq(2, filter.predicates.items.len);
-    try eqStr("variable.builtin", filter.predicates.items[0].eq.capture);
-    try eqStr("_", filter.predicates.items[0].eq.target);
-    try eqStr("include", filter.predicates.items[1].any_of.capture);
-    try eq(2, filter.predicates.items[1].any_of.targets.items.len);
-    try eqStr("@import", filter.predicates.items[1].any_of.targets.items[0]);
-    try eqStr("@cImport", filter.predicates.items[1].any_of.targets.items[1]);
+    try eq(3, filter.patterns.len);
+
+    try eq(1, filter.patterns[0].len);
+    try eqStr("variable.builtin", filter.patterns[0][0].eq.capture);
+    try eqStr("_", filter.patterns[0][0].eq.target);
+
+    try eq(1, filter.patterns[1].len);
+    try eqStr("include", filter.patterns[1][0].any_of.capture);
+    try eq(2, filter.patterns[1][0].any_of.targets.len);
+    try eqStr("@import", filter.patterns[1][0].any_of.targets[0]);
+    try eqStr("@cImport", filter.patterns[1][0].any_of.targets[1]);
+
+    try eq(2, filter.patterns[2].len);
+    try eqStr("contrived-example", filter.patterns[2][0].eq.capture);
+    try eqStr("@contrived", filter.patterns[2][0].eq.target);
+    try eq(.unsupported, filter.patterns[2][1].unsupported);
+
+    // _ = filter.nextMatch(source, cursor);
+    // _ = filter.nextMatch(source, cursor);
 }
