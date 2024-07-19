97237be6e03d907525022b341ea0e908106146cd
comrade-T <communism.app@gmail.com>
2024-07-20 01:02:12 +0700

    wip(buf): fill out skeleton of Node, Branch, Leaf

--- src/buffer/buffer_brave_new_world.zig
+++ src/buffer/buffer_brave_new_world.zig
@@ -8,9 +8,68 @@ const eqStr = std.testing.expectEqualStrings;
 
 //////////////////////////////////////////////////////////////////////////////////////////////
 
+const empty_leaf_node: Node = .{ .leaf = .{ .buf = "" } };
 const Node = union(enum) {
-    leaf: Leaf,
     branch: Branch,
+    leaf: Leaf,
+
+    fn new(a: Allocator, l: *const Node, r: *const Node) !*const Node {
+        const node = try a.create(Node);
+        var w = Weights{};
+        w.add(l.weights());
+        w.add(r.weights());
+        w.depth += 1;
+        node.* = .{ .branch = .{ .left = l, .right = r, .weights = w } };
+        return node;
+    }
+    test new {
+        var arena = std.heap.ArenaAllocator.init(std.testing.allocator);
+        defer arena.deinit();
+        const a = arena.allocator();
+        const node = try Node.new(arena.allocator(), try Leaf.new(a, "one"), try Leaf.new(a, " two"));
+        try eqStr("one", node.branch.left.leaf.buf);
+        try eqStr(" two", node.branch.right.leaf.buf);
+    }
+
+    fn weights(self: *const Node) Weights {
+        return switch (self.*) {
+            .branch => |*b| b.weights,
+            .leaf => |*l| l.weights(),
+        };
+    }
+    test weights {
+        var arena = std.heap.ArenaAllocator.init(std.testing.allocator);
+        defer arena.deinit();
+        const a = arena.allocator();
+        const node = try Node.new(arena.allocator(), try Leaf.new(a, "one"), try Leaf.new(a, " two"));
+        try eqDeep(Weights{ .len = 7, .depth = 2 }, node.weights());
+        try eqDeep(Weights{ .len = 3, .depth = 1 }, node.branch.left.weights());
+        try eqDeep(Weights{ .len = 4, .depth = 1 }, node.branch.right.weights());
+    }
+
+    fn isEmpty(self: *const Node) bool {
+        return switch (self.*) {
+            .branch => |*b| b.left.isEmpty() and b.right.isEmpty(),
+            .leaf => |*l| l.isEmpty(),
+        };
+    }
+    test isEmpty {
+        var arena = std.heap.ArenaAllocator.init(std.testing.allocator);
+        defer arena.deinit();
+        const a = arena.allocator();
+        {
+            const node = try Node.new(arena.allocator(), try Leaf.new(a, ""), try Leaf.new(a, ""));
+            try eq(true, node.isEmpty());
+            try eq(true, node.branch.left.isEmpty());
+            try eq(true, node.branch.right.isEmpty());
+        }
+        {
+            const node = try Node.new(arena.allocator(), try Leaf.new(a, ""), try Leaf.new(a, "two"));
+            try eq(false, node.isEmpty());
+            try eq(true, node.branch.left.isEmpty());
+            try eq(false, node.branch.right.isEmpty());
+        }
+    }
 };
 
 const Branch = struct {
@@ -19,30 +78,41 @@ const Branch = struct {
     weights: Weights,
 };
 
-const empty_leaf: Node = .{ .leaf = .{ .buf = "" } };
 const Leaf = struct {
     buf: []const u8,
 
     inline fn new(a: Allocator, source: []const u8) !*const Node {
-        if (source.len == 0) return &empty_leaf;
+        if (source.len == 0) return &empty_leaf_node;
         const node = try a.create(Node);
         node.* = .{ .leaf = .{ .buf = source } };
         return node;
     }
-
     test new {
-        var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+        var arena = std.heap.ArenaAllocator.init(std.testing.allocator);
         defer arena.deinit();
         const node = try Leaf.new(arena.allocator(), "hello");
         try eqStr("hello", node.leaf.buf);
     }
 
+    fn weights(self: *const Leaf) Weights {
+        return Weights{ .len = self.buf.len };
+    }
+    test weights {
+        {
+            const leaf = Leaf{ .buf = "" };
+            try eqDeep(Weights{ .len = 0, .depth = 1 }, leaf.weights());
+        }
+        {
+            const leaf = Leaf{ .buf = "hello" };
+            try eqDeep(Weights{ .len = 5, .depth = 1 }, leaf.weights());
+        }
+    }
+
     inline fn isEmpty(self: *const Leaf) bool {
         return self.buf.len == 0;
     }
-
     test isEmpty {
-        var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+        var arena = std.heap.ArenaAllocator.init(std.testing.allocator);
         defer arena.deinit();
         {
             const node = try Leaf.new(arena.allocator(), "hello");
@@ -54,20 +124,18 @@ const Leaf = struct {
         }
     }
 };
-
 test Leaf {
     _ = Leaf{ .buf = "" };
 }
 
 const Weights = struct {
-    len: u32 = 0,
-    depth: u32 = 1,
+    len: usize = 0,
+    depth: usize = 1,
 
     inline fn add(self: *Weights, other: Weights) void {
         self.len += other.len;
         self.depth = @max(self.depth, other.depth);
     }
-
     test add {
         {
             var w = Weights{};
@@ -81,7 +149,6 @@ const Weights = struct {
         }
     }
 };
-
 test Weights {
     _ = Weights{};
 }
