687e5c2e07f680ffac1d360e30926c6cb7dbd8a6
comrade-T <communism.app@gmail.com>
2024-07-24 21:13:07 +0700

    chore(buf): add docstring to structs and methods

--- src/buffer/buffer_brave_new_world.zig
+++ src/buffer/buffer_brave_new_world.zig
@@ -14,36 +14,42 @@ const idc_if_it_leaks = std.heap.page_allocator;
 
 //////////////////////////////////////////////////////////////////////////////////////////////
 
-const Walker = struct {
+/// Represents a result after walking through a Node.
+const WalkResult = struct {
     keep_walking: bool = false,
     found: bool = false,
     err: ?anyerror = null,
 
-    const keep_walking = Walker{ .keep_walking = true };
-    const stop = Walker{ .keep_walking = false };
-    const found = Walker{ .found = true };
+    const keep_walking = WalkResult{ .keep_walking = true };
+    const stop = WalkResult{ .keep_walking = false };
+    const found = WalkResult{ .found = true };
 
-    const F = *const fn (ctx: *anyopaque, node: *const Node) Walker;
+    const F = *const fn (ctx: *anyopaque, node: *const Node) WalkResult;
 
-    fn mergeWalkResults(left: Walker, right: Walker) Walker {
-        var result = Walker{};
-        result.err = if (left.err) |_| left.err else right.err;
-        result.keep_walking = left.keep_walking and right.keep_walking;
-        result.found = left.found or right.found;
+    /// Produce a merged walk result from `self` and another WalkResult.
+    fn merge(self: WalkResult, right: WalkResult) WalkResult {
+        var result = WalkResult{};
+        result.err = if (self.err) |_| self.err else right.err;
+        result.keep_walking = self.keep_walking and right.keep_walking;
+        result.found = self.found or right.found;
         return result;
     }
-    test mergeWalkResults {
-        try eqDeep(Walker.found, mergeWalkResults(Walker.found, Walker.keep_walking));
-        try eqDeep(Walker.keep_walking, mergeWalkResults(Walker.keep_walking, Walker.keep_walking));
-        try eqDeep(Walker.stop, mergeWalkResults(Walker.stop, Walker.keep_walking));
+    test merge {
+        try eqDeep(WalkResult.found, merge(WalkResult.found, WalkResult.keep_walking));
+        try eqDeep(WalkResult.keep_walking, merge(WalkResult.keep_walking, WalkResult.keep_walking));
+        try eqDeep(WalkResult.stop, merge(WalkResult.stop, WalkResult.keep_walking));
     }
 };
 
 const empty_leaf_node: Node = .{ .leaf = .{ .buf = "" } };
+
+/// Primary data structure to manage an editable text buffer.
+/// Can either be a Branch or a Leaf.
 const Node = union(enum) {
     branch: Branch,
     leaf: Leaf,
 
+    /// Create a new Branch node, given a `left` Node and a `right` Node.
     fn new(a: Allocator, left: *const Node, right: *const Node) !*const Node {
         const node = try a.create(Node);
         var w = Weights{};
@@ -56,9 +62,10 @@ const Node = union(enum) {
 
     ///////////////////////////// Load
 
-    pub fn fromString(a: Allocator, s: []const u8) !*const Node {
-        var stream = std.io.fixedBufferStream(s);
-        return Node.fromReader(a, stream.reader(), s.len);
+    /// Create 1 single Leaf Node given string argument.
+    pub fn fromString(a: Allocator, source: []const u8) !*const Node {
+        var stream = std.io.fixedBufferStream(source);
+        return Node.fromReader(a, stream.reader(), source.len);
     }
     test fromString {
         var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
@@ -71,6 +78,7 @@ const Node = union(enum) {
         }
     }
 
+    /// Create 1 single Leaf Node from the source read from the `reader`.
     fn fromReader(a: Allocator, reader: anytype, buffer_size: usize) !*const Node {
         const buf = try a.alloc(u8, buffer_size);
 
@@ -83,15 +91,18 @@ const Node = union(enum) {
         return Leaf.new(a, buf);
     }
 
-    /// Currently unused, due to author trying to experiment with not splitting input chars at all.
-    fn createLeavesByCapacity(a: Allocator, buf: []const u8, capacity_per_leaf: usize) ![]Node {
+    /// Create Leaves from given `source`. Each Leaf.buf's length restricted to `capacity_per_leaf`.
+    ///
+    /// Currently unused, due to author trying to experiment not splitting the source at all.
+    /// Instead in favor of creating 1 single Leaf from the source.
+    fn createLeavesByCapacity(a: Allocator, source: []const u8, capacity_per_leaf: usize) ![]Node {
         var leaf_count: usize = 1;
         var split_indexes = try std.ArrayList(usize).initCapacity(a, 8);
         defer split_indexes.deinit();
 
         var i: usize = 0;
         var j: usize = 0;
-        while (i < buf.len) {
+        while (i < source.len) {
             if (j >= capacity_per_leaf and i < 128) {
                 j = 0;
                 leaf_count += 1;
@@ -100,13 +111,13 @@ const Node = union(enum) {
             i += 1;
             j += 1;
         }
-        try split_indexes.append(buf.len);
+        try split_indexes.append(source.len);
 
         var leaves = try a.alloc(Node, leaf_count);
         var cur_leaf: usize = 0;
         var start: usize = 0;
         for (split_indexes.items) |end| {
-            leaves[cur_leaf] = .{ .leaf = .{ .buf = buf[start..end] } };
+            leaves[cur_leaf] = .{ .leaf = .{ .buf = source[start..end] } };
             cur_leaf += 1;
             start = end;
         }
@@ -124,6 +135,7 @@ const Node = union(enum) {
         }
     }
 
+    /// Recursively create and return Nodes given a slice of Leaves.
     fn mergeLeaves(a: Allocator, leaves: []const Node) !*const Node {
         if (leaves.len == 1) return &leaves[0];
         if (leaves.len == 2) return Node.new(a, &leaves[0], &leaves[1]);
@@ -152,10 +164,10 @@ const Node = union(enum) {
     fn testNodesTraversed(root: *const Node, expected: []struct { *const Node, ?[]const u8 }) !void {
         const TestTraverseNodesCtx = struct {
             nodes: *ArrayList(*const Node),
-            fn walker(ctx_: *anyopaque, node: *const Node) Walker {
+            fn walker(ctx_: *anyopaque, node: *const Node) WalkResult {
                 const ctx = @as(*@This(), @ptrCast(@alignCast(ctx_)));
-                ctx.nodes.append(node) catch |e| return Walker{ .err = e };
-                return Walker.keep_walking;
+                ctx.nodes.append(node) catch |e| return WalkResult{ .err = e };
+                return WalkResult.keep_walking;
             }
         };
 
@@ -164,7 +176,7 @@ const Node = union(enum) {
         var ctx: TestTraverseNodesCtx = .{ .nodes = &collected_nodes };
         const walk_result = root.walk(TestTraverseNodesCtx.walker, &ctx);
 
-        try eq(Walker.keep_walking, walk_result);
+        try eq(WalkResult.keep_walking, walk_result);
         try eq(expected.len, collected_nodes.items.len);
         for (expected, 0..) |e, i| {
             const expected_node, const may_expected_str = e;
@@ -175,7 +187,7 @@ const Node = union(enum) {
 
     /// Recursively walk the Node and execute walker callback function on every Node encountered,
     /// regardless if it's a Branch or a Leaf.
-    fn walk(self: *const Node, f: Walker.F, ctx: *anyopaque) Walker {
+    fn walk(self: *const Node, f: WalkResult.F, ctx: *anyopaque) WalkResult {
         const current = f(ctx, self);
         switch (self.*) {
             .branch => |*branch| {
@@ -185,7 +197,7 @@ const Node = union(enum) {
                 if (left.found) return left;
 
                 const right = branch.right.walk(f, ctx);
-                return Walker.mergeWalkResults(left, right);
+                return left.merge(right);
             },
 
             .leaf => |_| return current,
@@ -214,6 +226,8 @@ const Node = union(enum) {
 
     ///////////////////////////// Node Info
 
+    /// Either returns a Branch's `weights` field value
+    /// or a Leaf.buffer's length as `Weights`.
     fn weights(self: *const Node) Weights {
         return switch (self.*) {
             .branch => |*b| b.weights,
@@ -232,6 +246,7 @@ const Node = union(enum) {
         try eqDeep(Weights{ .len = 4, .depth = 1 }, node.branch.right.weights());
     }
 
+    /// Recursively walk through self and check if all the leaves' buffers are empty.
     fn isEmpty(self: *const Node) bool {
         return switch (self.*) {
             .branch => |*b| b.left.isEmpty() and b.right.isEmpty(),
@@ -258,6 +273,7 @@ const Node = union(enum) {
 
     ///////////////////////////// Debug Print
 
+    /// Prints a Node's info.
     fn debugPrint(self: *const Node) void {
         switch (self.*) {
             .branch => std.debug.print("Branch: depth: {d} | len: {d}\n", .{ self.weights().depth, self.weights().len }),
@@ -266,15 +282,20 @@ const Node = union(enum) {
     }
 };
 
+/// A Branch contains pointers to its left and right Nodes.
+/// Its `weights.len` has the sum value of its left and right Nodes.
+/// Its `weights.depth` has the max value of its left and right Nodes +1.
 const Branch = struct {
     left: *const Node,
     right: *const Node,
     weights: Weights,
 };
 
+/// A Leaf's only job is to have a `buf` field that points to a slice of bytes.
 const Leaf = struct {
     buf: []const u8,
 
+    /// Create a Leaf Node with a `buf` field that points to a slice of bytes.
     inline fn new(a: Allocator, source: []const u8) !*const Node {
         if (source.len == 0) return &empty_leaf_node;
         const node = try a.create(Node);
@@ -292,6 +313,7 @@ const Leaf = struct {
         }
     }
 
+    /// Returns a `Weights` object with depth of 1 and len of self.buf.len
     fn weights(self: *const Leaf) Weights {
         return Weights{ .len = self.buf.len };
     }
@@ -306,6 +328,7 @@ const Leaf = struct {
         }
     }
 
+    /// Returns `true` if self.buf.len == 0
     inline fn isEmpty(self: *const Leaf) bool {
         return self.buf.len == 0;
     }
@@ -326,10 +349,14 @@ test Leaf {
     _ = Leaf{ .buf = "" };
 }
 
+/// Contains 2 fields: `len` and `depth`.
 const Weights = struct {
     len: usize = 0,
     depth: usize = 1,
 
+    /// Mutates `self` in place.
+    /// Mutated length is self.len + other.len.
+    /// Mutated depth is max value of self.depth vs other.depth.
     inline fn add(self: *Weights, other: Weights) void {
         self.len += other.len;
         self.depth = @max(self.depth, other.depth);
