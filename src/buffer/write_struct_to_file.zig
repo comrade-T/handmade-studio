c8ba7f2de7a90dbc341bd857ae4be4c92a3bcbfe
comrade-T <communism.app@gmail.com>
2024-07-30 19:20:51 +0700

    wip(tofile): add struct with string field test case

--- src/buffer/write_struct_to_file.zig
+++ src/buffer/write_struct_to_file.zig
@@ -3,7 +3,7 @@ const s2s = @import("s2s");
 
 //////////////////////////////////////////////////////////////////////////////////////////////
 
-fn testSerDesAlloc(comptime cmp_type: enum { eq, eqSlices, eqPtrContent }, comptime T: type, value: T) !void {
+fn testSerDesAlloc(comptime cmp_type: enum { eq, eqSlices, eqSlicesStringContent, eqPtrContent }, comptime T: type, value: T) !void {
     var data = std.ArrayList(u8).init(std.testing.allocator);
     defer data.deinit();
 
@@ -16,11 +16,75 @@ fn testSerDesAlloc(comptime cmp_type: enum { eq, eqSlices, eqPtrContent }, compt
     switch (cmp_type) {
         .eq => try std.testing.expectEqual(value, deserialized),
         .eqSlices => try std.testing.expectEqualSlices(std.meta.Child(T), value, deserialized),
+        .eqSlicesStringContent => {
+            try std.testing.expectEqual(value.len, deserialized.len);
+            for (0..value.len) |i| try std.testing.expectEqualStrings(value[i], deserialized[i]);
+        },
         .eqPtrContent => try std.testing.expectEqual(value.*, deserialized.*),
     }
 }
 
-test "ser/des" {
+test "see how it prints in bytes" {
+    // {
+    //     var data = std.ArrayList(u8).init(std.testing.allocator);
+    //     defer data.deinit();
+    //     try s2s.serialize(data.writer(), []const u8, "Billie Jean");
+    //     std.debug.print("{s}\n", .{data.items});
+    // }
+    // {
+    //     const MyStruct = struct { x: u32, y: u32, content: []const u8 };
+    //     var data = std.ArrayList(u8).init(std.testing.allocator);
+    //     defer data.deinit();
+    //     try s2s.serialize(data.writer(), MyStruct, MyStruct{ .x = 10, .y = 20, .content = "Billie Jean" });
+    //     std.debug.print("{s}\n", .{data.items});
+    // }
+    // {
+    //     const MyStruct = struct { x: u32, y: u32, content: []const u8 };
+    //     var data = std.ArrayList(u8).init(std.testing.allocator);
+    //     defer data.deinit();
+    //
+    //     var list = std.ArrayList(MyStruct).init(std.testing.allocator);
+    //     defer list.deinit();
+    //     try list.append(.{ .x = 10, .y = 20, .content = "Billie Jean" });
+    //     try list.append(.{ .x = 20, .y = 30, .content = "Coming of Age Ceremony" });
+    //     try list.append(.{ .x = 18, .y = 23, .content = "We are strangers now" });
+    //
+    //     try s2s.serialize(data.writer(), []MyStruct, list.items);
+    //     std.debug.print("{s}\n", .{data.items});
+    // }
+}
+
+test "custom ser/des" {
+    const a = std.testing.allocator;
+    { // [][]const u8
+        var list = std.ArrayList([]const u8).init(a);
+        defer list.deinit();
+        try list.append("one");
+        try list.append("two");
+        try list.append("three");
+        try testSerDesAlloc(.eqSlicesStringContent, [][]const u8, list.items);
+    }
+    { // []Vec2
+        const Vec2 = struct { x: u32, y: u32 };
+        var list = std.ArrayList(Vec2).init(a);
+        defer list.deinit();
+        try list.append(.{ .x = 10, .y = 20 });
+        try list.append(.{ .x = 30, .y = 40 });
+        try list.append(.{ .x = 50, .y = 60 });
+        try testSerDesAlloc(.eqSlices, []Vec2, list.items);
+    }
+    // { // []MyStruct // they're equal in values, just different in pointers
+    //     const MyStruct = struct { x: u32, y: u32, content: []const u8 };
+    //     var list = std.ArrayList(MyStruct).init(a);
+    //     defer list.deinit();
+    //     try list.append(.{ .x = 10, .y = 20, .content = "Billie Jean" });
+    //     try list.append(.{ .x = 20, .y = 30, .content = "Coming of Age Ceremony" });
+    //     try list.append(.{ .x = 18, .y = 23, .content = "We are strangers now" });
+    //     try testSerDesAlloc(.eqSlices, []MyStruct, list.items);
+    // }
+}
+
+test "ser/des in s2s.zig" {
     try testSerDesAlloc(.eq, void, {});
     try testSerDesAlloc(.eq, bool, false);
     try testSerDesAlloc(.eq, bool, true);
@@ -69,7 +133,7 @@ test "ser/des" {
     try testSerDesAlloc(.eq, ?u32, 143);
 }
 
-test "fails to compile" {
+test "cases that failed to compile in s2s.zig" {
     // // It seems like I can't serialize error values.
 
     // try testSerDesAlloc(struct { val: error{ Foo, Bar } }, .{ .val = error.Foo });
