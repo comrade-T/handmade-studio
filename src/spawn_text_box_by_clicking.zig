534e5a8f6197d98057f4ca9a4a16ea11cd0059a6
comrade-T <communism.app@gmail.com>
2024-08-05 17:21:29 +0700

    wip(ugly): add FileNavigator.backwards()

--- src/spawn_text_box_by_clicking.zig
+++ src/spawn_text_box_by_clicking.zig
@@ -17,17 +17,17 @@ const FileNavigator = struct {
     exa: Allocator,
     arena: std.heap.ArenaAllocator,
 
-    current_short_paths: [][]const u8,
+    short_paths: [][]const u8,
     history: ArrayList(ArrayList(u8)),
-    current_index: usize,
+    index: usize,
 
     fn new(external_allocator: Allocator) !*@This() {
         var self = try external_allocator.create(@This());
         self.exa = external_allocator;
         self.arena = std.heap.ArenaAllocator.init(self.exa);
-        self.current_short_paths = fs.getFileNamesRelativeToCwd(self.arena.allocator(), ".");
+        self.short_paths = fs.getFileNamesRelativeToCwd(self.arena.allocator(), ".");
         self.history = ArrayList(ArrayList(u8)).init(self.exa);
-        self.current_index = 0;
+        self.index = 0;
         return self;
     }
     fn deinit(self: *@This()) void {
@@ -37,7 +37,7 @@ const FileNavigator = struct {
         self.exa.destroy(self);
     }
 
-    fn getRelativePath(self: *@This()) !ArrayList(u8) {
+    fn getCurrentRelativePath(self: *@This()) !ArrayList(u8) {
         var result = std.ArrayList(u8).init(self.exa);
         if (self.history.items.len == 0) {
             try result.appendSlice("./");
@@ -48,10 +48,26 @@ const FileNavigator = struct {
         return result;
     }
 
+    fn backwards(self: *@This()) !void {
+        if (self.history.items.len == 0) return;
+        const last_history = self.history.pop();
+        last_history.deinit();
+
+        const target_path = try self.getCurrentRelativePath();
+        defer target_path.deinit();
+
+        self.arena.deinit();
+        self.arena = std.heap.ArenaAllocator.init(self.exa);
+
+        const new_short_paths = fs.getFileNamesRelativeToCwd(self.arena.allocator(), target_path.items);
+        self.short_paths = new_short_paths;
+        self.index = 0;
+    }
+
     fn forward(self: *@This()) !?ArrayList(u8) {
-        const current_relative_path = try self.getRelativePath();
+        const current_relative_path = try self.getCurrentRelativePath();
         defer current_relative_path.deinit();
-        const current_short_path = self.current_short_paths[self.current_index];
+        const current_short_path = self.short_paths[self.index];
 
         var new_relative_path = std.ArrayList(u8).init(self.exa);
         try new_relative_path.appendSlice(current_relative_path.items);
@@ -61,9 +77,9 @@ const FileNavigator = struct {
             self.arena.deinit();
             self.arena = std.heap.ArenaAllocator.init(self.exa);
 
-            const new_current_paths = fs.getFileNamesRelativeToCwd(self.arena.allocator(), new_relative_path.items);
-            self.current_short_paths = new_current_paths;
-            self.current_index = 0;
+            const new_short_paths = fs.getFileNamesRelativeToCwd(self.arena.allocator(), new_relative_path.items);
+            self.short_paths = new_short_paths;
+            self.index = 0;
 
             try self.history.append(new_relative_path);
             return null;
@@ -73,10 +89,10 @@ const FileNavigator = struct {
     }
 
     fn moveUp(self: *@This()) void {
-        self.current_index = self.current_index -| 1;
+        self.index = self.index -| 1;
     }
     fn moveDown(self: *@This()) void {
-        if (self.current_index + 1 < self.current_short_paths.len) self.current_index = self.current_index + 1;
+        if (self.index + 1 < self.short_paths.len) self.index = self.index + 1;
     }
 };
 
@@ -171,6 +187,7 @@ pub fn main() anyerror!void {
                                 defer path.deinit();
                             }
                         }
+                        if (eql(u8, trigger, "lctrl h")) try navigator.backwards();
                     }
 
                     try triggerCallback(&trigger_map, trigger, active_buf);
@@ -206,11 +223,11 @@ pub fn main() anyerror!void {
                 }
             }
             {
-                for (navigator.current_short_paths, 0..) |path, i| {
+                for (navigator.short_paths, 0..) |path, i| {
                     const text = try std.fmt.allocPrintZ(gpa, "{s}", .{path});
                     defer gpa.free(text);
                     const idx: i32 = @intCast(i);
-                    const color = if (i == navigator.current_index) rl.Color.sky_blue else rl.Color.ray_white;
+                    const color = if (i == navigator.index) rl.Color.sky_blue else rl.Color.ray_white;
                     rl.drawText(text, 100, 100 + idx * 40, 30, color);
                 }
             }
