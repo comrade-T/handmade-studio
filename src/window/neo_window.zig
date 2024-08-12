bf8c65bd467e23675b10f86fe67f608132afdda7
comrade-T <communism.app@gmail.com>
2024-08-13 01:38:44 +0700

    wip(win): add setup & teardown test helpers

--- src/window/neo_window.zig
+++ src/window/neo_window.zig
@@ -63,66 +63,97 @@ pub const Window = struct {
     }
 
     pub fn doCustomStuffs(self: *@This(), trigger: []const u8) !void {
-        if (eql(u8, trigger, "up")) self.cursor.up(1);
-        if (eql(u8, trigger, "down")) self.cursor.down(1, self.vendor.buffer.roperoot.weights().bols);
-        if (eql(u8, trigger, "left")) self.cursor.left(1);
-        if (eql(u8, trigger, "right")) {
-            const buf_size = 1;
-            var buf: [buf_size]u8 = undefined;
-            const start_byte = try self.vendor.buffer.roperoot.getByteOffsetOfPosition(self.cursor.line, self.cursor.col);
-            const content, const eol = self.vendor.buffer.roperoot.getRestOfLine(start_byte, &buf, buf_size);
-            if (eol)
-                self.cursor.set(self.cursor.line + 1, 0)
-            else if (content.len > 0)
-                self.cursor.set(self.cursor.line, self.cursor.col + 1);
-        }
+        if (eql(u8, trigger, "up")) self.moveLeft();
+        if (eql(u8, trigger, "down")) self.moveDown();
+        if (eql(u8, trigger, "left")) self.moveLeft();
+        if (eql(u8, trigger, "right")) try self.moveRight();
     }
+
+    ///////////////////////////// Cursor Movement
+
+    fn moveUp(self: *@This()) void {
+        self.cursor.up(1);
+    }
+
+    fn moveDown(self: *@This()) void {
+        self.cursor.down(1, self.vendor.buffer.roperoot.weights().bols);
+    }
+
+    fn moveLeft(self: *@This()) void {
+        self.cursor.left(1);
+    }
+
+    fn moveRight(self: *@This()) !void {
+        const buf_size = 1;
+        var buf: [buf_size]u8 = undefined;
+        const start_byte = try self.vendor.buffer.roperoot.getByteOffsetOfPosition(self.cursor.line, self.cursor.col);
+        const content, const eol = self.vendor.buffer.roperoot.getRestOfLine(start_byte, &buf, buf_size);
+        if (eol)
+            self.cursor.set(self.cursor.line + 1, 0)
+        else if (content.len > 0)
+            self.cursor.set(self.cursor.line, self.cursor.col + 1);
+    }
+    test moveRight {
+        const win = try setupZigWindow("");
+        defer teardownWindow(win);
+
+        // TODO:
+    }
+
+    ///////////////////////////// Insert
 
     fn insertCharsInternal(self: *@This(), chars: []const u8) !void {
         const new_line, const new_col = try self.vendor.buffer.insertChars(chars, self.cursor.line, self.cursor.col);
         self.cursor.set(new_line, new_col);
     }
     test insertCharsInternal {
-        var buf = try Buffer.create(testing_allocator, .string, "");
-        try buf.initiateTreeSitter(.zig);
-        defer buf.destroy();
-
-        const vendor = try ContentVendor.init(testing_allocator, buf);
-        defer vendor.deinit();
-
-        const win = try Window.spawn(testing_allocator, vendor, 100, 100);
-        defer win.destroy();
-
+        const win = try setupZigWindow("");
+        defer teardownWindow(win);
         {
             win.insertChars("c");
-            const iter = try vendor.requestLines(0, 9999);
+            const iter = try win.vendor.requestLines(0, 9999);
             defer iter.deinit();
             try testIter(iter, "c", "variable");
         }
         {
             win.insertChars("o");
-            const iter = try vendor.requestLines(0, 9999);
+            const iter = try win.vendor.requestLines(0, 9999);
             defer iter.deinit();
             try testIter(iter, "co", "variable");
         }
         {
             win.insertChars("n");
-            const iter = try vendor.requestLines(0, 9999);
+            const iter = try win.vendor.requestLines(0, 9999);
             defer iter.deinit();
             try testIter(iter, "con", "variable");
         }
         {
             win.insertChars("s");
-            const iter = try vendor.requestLines(0, 9999);
+            const iter = try win.vendor.requestLines(0, 9999);
             defer iter.deinit();
             try testIter(iter, "cons", "variable");
         }
         {
             win.insertChars("t");
-            const iter = try vendor.requestLines(0, 9999);
+            const iter = try win.vendor.requestLines(0, 9999);
             defer iter.deinit();
             try testIter(iter, "const", "type.qualifier");
         }
+    }
+
+    ///////////////////////////// Test Helpers
+
+    fn setupZigWindow(source: []const u8) !*@This() {
+        var buf = try Buffer.create(testing_allocator, .string, source);
+        try buf.initiateTreeSitter(.zig);
+        const vendor = try ContentVendor.init(testing_allocator, buf);
+        const win = try Window.spawn(testing_allocator, vendor, 100, 100);
+        return win;
+    }
+    fn teardownWindow(win: *Window) void {
+        win.vendor.buffer.destroy();
+        win.vendor.deinit();
+        win.destroy();
     }
 };
 
