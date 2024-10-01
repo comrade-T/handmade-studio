dc391d4347644f73cb0053572c679f09e49f5524
comrade-T <communism.app@gmail.com>
2024-10-02 04:00:07 +0700

    wip(dcp): add Display struct

--- src/window/DisplayCachePool.zig
+++ src/window/DisplayCachePool.zig
@@ -15,6 +15,7 @@ const testing_allocator = std.testing.allocator;
 const eql = std.mem.eql;
 const eq = std.testing.expectEqual;
 const eqStr = std.testing.expectEqualStrings;
+const shouldErr = std.testing.expectError;
 const assert = std.debug.assert;
 
 //////////////////////////////////////////////////////////////////////////////////////////////
@@ -22,7 +23,11 @@ const assert = std.debug.assert;
 a: Allocator,
 buf: *Buffer,
 
-fn init(a: Allocator, buf: *Buffer) !*DisplayCachePool {
+start_line: usize = 0,
+end_line: usize = 0,
+
+const InitError = error{OutOfMemory};
+pub fn init(a: Allocator, buf: *Buffer) InitError!*DisplayCachePool {
     const self = try a.create(@This());
     self.* = DisplayCachePool{
         .a = a,
@@ -31,16 +36,81 @@ fn init(a: Allocator, buf: *Buffer) !*DisplayCachePool {
     return self;
 }
 
-fn deinit(self: *@This()) void {
+pub fn deinit(self: *@This()) void {
     self.a.destroy(self);
 }
 
-//////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////// requestLines
 
-test init {
+const RequestLinesError = error{ OutOfMemory, EndLineOutOfBounds };
+const RequestLinesResult = struct { []u21, []Display };
+pub fn requestLines(self: *@This(), start: usize, end: usize) RequestLinesError!RequestLinesResult {
+    if (end > self.lastLineNumber()) return RequestLinesError.EndLineOutOfBounds;
+    _ = start;
+    return .{ &.{}, &.{} };
+}
+
+test requestLines {
     var buf = try Buffer.create(testing_allocator, .string, "hello world");
     defer buf.destroy();
 
     var dcp = try DisplayCachePool.init(testing_allocator, buf);
     defer dcp.deinit();
+
+    try shouldErr(RequestLinesError.EndLineOutOfBounds, dcp.requestLines(0, 1));
 }
+
+///////////////////////////// updateLines
+
+// TODO:
+
+///////////////////////////// infos
+
+fn lastLineNumber(self: *@This()) usize {
+    return self.numberOfLines() - 1;
+}
+
+fn numberOfLines(self: *@This()) usize {
+    return self.buf.roperoot.weights().bols;
+}
+
+fn getWidth(self: *@This()) f32 {
+    _ = self;
+    unreachable;
+}
+
+fn getHeight(self: *@This()) f32 {
+    _ = self;
+    unreachable;
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////////
+
+const CellIndex = struct { line: usize, col: usize };
+const Display = struct {
+    width: f32,
+    height: f32,
+    variant: union(enum) {
+        const Char = struct {
+            font_size: f32,
+            font_face: []const u8,
+            color: u32,
+        };
+        const Image = struct {
+            path: []const u8,
+        };
+        const Conceal = struct {
+            start: CellIndex,
+            end: CellIndex,
+            variant: union(enum) {
+                char: Char,
+                image: Image,
+            },
+        };
+        char: Char,
+        image: Image,
+        conceal: Conceal,
+    },
+};
+
+//////////////////////////////////////////////////////////////////////////////////////////////
