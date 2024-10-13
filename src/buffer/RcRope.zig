ce5885daccc28e597d5e24971349e1efe493db8a
comrade-T <communism.app@gmail.com>
2024-10-13 20:11:18 +0700

    ref(RcR): make Node.new() callers call retain() themselves

--- src/buffer/RcRope.zig
+++ src/buffer/RcRope.zig
@@ -32,18 +32,16 @@ pub const WalkMutResult = struct {
         var result = WalkMutResult{};
         result.err = if (left.err) |_| left.err else right.err;
         if (left.replace != null or right.replace != null) {
-            var new_left = branch.left;
-            var retain_left = true;
-            if (left.replace) |replacement| {
-                new_left = replacement;
-                retain_left = false;
+            var new_left: RcNode = undefined;
+            if (left.replace) |replacement| new_left = replacement else {
+                var branch_left_clone = branch.left;
+                new_left = branch_left_clone.retain();
             }
 
-            var new_right = branch.right;
-            var retain_right = true;
-            if (right.replace) |replacement| {
-                new_right = replacement;
-                retain_right = false;
+            var new_right: RcNode = undefined;
+            if (right.replace) |replacement| new_right = replacement else {
+                var branch_right_clone = branch.right;
+                new_right = branch_right_clone.retain();
             }
 
             result.replace = if (new_left.value.isEmpty())
@@ -51,7 +49,7 @@ pub const WalkMutResult = struct {
             else if (new_right.value.isEmpty())
                 new_left
             else
-                try Node.new(a, &new_left, retain_left, &new_right, retain_right);
+                try Node.new(a, new_left, new_right);
         }
         result.keep_walking = left.keep_walking and right.keep_walking;
         result.found = left.found or right.found;
@@ -64,7 +62,7 @@ fn walkMutFromLineBegin(a: Allocator, node: RcNode, line: usize, f: WalkMutCallb
         .branch => |*branch| {
             const left_bols = node.value.weights().bols;
             if (line >= left_bols) {
-                var right_result = try walkMutFromLineBegin(a, branch.right, line - left_bols, f, ctx);
+                const right_result = try walkMutFromLineBegin(a, branch.right, line - left_bols, f, ctx);
                 if (right_result.replace) |replacement| {
                     var result = WalkMutResult{};
                     result.err = right_result.err;
@@ -73,7 +71,7 @@ fn walkMutFromLineBegin(a: Allocator, node: RcNode, line: usize, f: WalkMutCallb
                     result.replace = if (replacement.value.isEmpty())
                         branch.left
                     else
-                        try Node.new(a, &branch.left, true, &right_result.replace.?, false);
+                        try Node.new(a, branch.left.retain(), right_result.replace.?);
                     return result;
                 }
                 return right_result;
@@ -104,13 +102,11 @@ const Node = union(enum) {
     branch: Branch,
     leaf: Leaf,
 
-    fn new(a: Allocator, left_: *RcNode, retain_left: bool, right_: *RcNode, retain_right: bool) !RcNode {
+    fn new(a: Allocator, left: RcNode, right: RcNode) !RcNode {
         var w = Weights{};
-        w.add(left_.value.weights());
-        w.add(right_.value.weights());
+        w.add(left.value.weights());
+        w.add(right.value.weights());
         w.depth += 1;
-        const left = if (retain_left) left_.retain() else left_.*;
-        const right = if (retain_right) right_.retain() else right_.*;
         return try RcNode.init(a, .{
             .branch = .{ .left = left, .right = right, .weights = w },
         });
@@ -234,12 +230,9 @@ const Node = union(enum) {
 
     fn mergeLeaves(a: Allocator, leaves: []RcNode) !RcNode {
         if (leaves.len == 1) return leaves[0];
-        if (leaves.len == 2) return Node.new(a, &leaves[0], false, &leaves[1], false);
-
+        if (leaves.len == 2) return Node.new(a, leaves[0], leaves[1]);
         const mid = leaves.len / 2;
-        var left = try mergeLeaves(a, leaves[0..mid]);
-        var right = try mergeLeaves(a, leaves[mid..]);
-        return Node.new(a, &left, false, &right, false);
+        return Node.new(a, try mergeLeaves(a, leaves[0..mid]), try mergeLeaves(a, leaves[mid..]));
     }
 
     ///////////////////////////// Insert
@@ -260,44 +253,44 @@ const Node = union(enum) {
             ctx.abs_col += leaf_noc;
 
             if (ctx.col == 0) {
-                var left = try Leaf.new(ctx.a, ctx.chars, leaf.bol, ctx.eol);
-                var right = try Leaf.new(ctx.a, leaf.buf, ctx.eol, leaf.eol);
-                return WalkMutResult{ .replace = try Node.new(ctx.a, &left, false, &right, false) };
+                const left = try Leaf.new(ctx.a, ctx.chars, leaf.bol, ctx.eol);
+                const right = try Leaf.new(ctx.a, leaf.buf, ctx.eol, leaf.eol);
+                return WalkMutResult{ .replace = try Node.new(ctx.a, left, right) };
             }
 
             if (leaf_noc == ctx.col) {
                 if (leaf.eol and ctx.eol and ctx.chars.len == 0) {
-                    var left = try Leaf.new(ctx.a, leaf.buf, leaf.bol, true);
-                    var right = try Leaf.new(ctx.a, ctx.chars, true, true);
-                    return WalkMutResult{ .replace = try Node.new(ctx.a, &left, false, &right, false) };
+                    const left = try Leaf.new(ctx.a, leaf.buf, leaf.bol, true);
+                    const right = try Leaf.new(ctx.a, ctx.chars, true, true);
+                    return WalkMutResult{ .replace = try Node.new(ctx.a, left, right) };
                 }
 
-                var left = try Leaf.new(ctx.a, leaf.buf, leaf.bol, false);
+                const left = try Leaf.new(ctx.a, leaf.buf, leaf.bol, false);
 
                 if (ctx.eol) {
-                    var middle = try Leaf.new(ctx.a, ctx.chars, false, ctx.eol);
-                    var right = try Leaf.new(ctx.a, "", ctx.eol, leaf.eol);
-                    var mid_right = try Node.new(ctx.a, &middle, false, &right, false);
-                    return WalkMutResult{ .replace = try Node.new(ctx.a, &left, false, &mid_right, false) };
+                    const middle = try Leaf.new(ctx.a, ctx.chars, false, ctx.eol);
+                    const right = try Leaf.new(ctx.a, "", ctx.eol, leaf.eol);
+                    const mid_right = try Node.new(ctx.a, middle, right);
+                    return WalkMutResult{ .replace = try Node.new(ctx.a, left, mid_right) };
                 }
 
-                var right = try Leaf.new(ctx.a, ctx.chars, false, leaf.eol);
-                return WalkMutResult{ .replace = try Node.new(ctx.a, &left, false, &right, false) };
+                const right = try Leaf.new(ctx.a, ctx.chars, false, leaf.eol);
+                return WalkMutResult{ .replace = try Node.new(ctx.a, left, right) };
             }
 
             if (leaf_noc > ctx.col) {
                 const pos = getNumOfBytesTillCol(leaf.buf, base_col);
                 if (ctx.eol and ctx.chars.len == 0) {
-                    var left = try Leaf.new(ctx.a, leaf.buf[0..pos], leaf.bol, ctx.eol);
-                    var right = try Leaf.new(ctx.a, leaf.buf[pos..], ctx.eol, leaf.eol);
-                    return WalkMutResult{ .replace = try Node.new(ctx.a, &left, false, &right, false) };
+                    const left = try Leaf.new(ctx.a, leaf.buf[0..pos], leaf.bol, ctx.eol);
+                    const right = try Leaf.new(ctx.a, leaf.buf[pos..], ctx.eol, leaf.eol);
+                    return WalkMutResult{ .replace = try Node.new(ctx.a, left, right) };
                 }
 
-                var left = try Leaf.new(ctx.a, leaf.buf[0..pos], leaf.bol, false);
-                var middle = try Leaf.new(ctx.a, ctx.chars, false, ctx.eol);
-                var right = try Leaf.new(ctx.a, leaf.buf[pos..], ctx.eol, leaf.eol);
-                var mid_right = try Node.new(ctx.a, &middle, false, &right, false);
-                return WalkMutResult{ .replace = try Node.new(ctx.a, &left, false, &mid_right, false) };
+                const left = try Leaf.new(ctx.a, leaf.buf[0..pos], leaf.bol, false);
+                const middle = try Leaf.new(ctx.a, ctx.chars, false, ctx.eol);
+                const right = try Leaf.new(ctx.a, leaf.buf[pos..], ctx.eol, leaf.eol);
+                const mid_right = try Node.new(ctx.a, middle, right);
+                return WalkMutResult{ .replace = try Node.new(ctx.a, left, mid_right) };
             }
 
             ctx.col -= leaf_noc;
@@ -394,7 +387,7 @@ const Node = union(enum) {
             var content_arena = std.heap.ArenaAllocator.init(testing_allocator);
             defer content_arena.deinit();
 
-            // init
+            // before
             const old_root = try Node.fromString(testing_allocator, &content_arena, "hello\nworld", true);
             try eqStr(
                 \\2 2/11
@@ -404,19 +397,22 @@ const Node = union(enum) {
 
             // after insertChars()
             const line, const col, const new_root = try insertChars(old_root, testing_allocator, &content_arena, "ok ", .{ .line = 0, .col = 0 });
-            try eqStr(
-                \\2 2/11
-                \\  1 B| `hello` |E
-                \\  1 B| `world` Rc:2
-            , try debugStr(idc_if_it_leaks, old_root));
-            try eq(.{ 0, 3 }, .{ line, col });
-            try eqStr(
-                \\3 2/14
-                \\  2 1/9
-                \\    1 B| `ok `
-                \\    1 `hello` |E
-                \\  1 B| `world` Rc:2
-            , try debugStr(idc_if_it_leaks, new_root));
+            {
+                try eqStr(
+                    \\2 2/11
+                    \\  1 B| `hello` |E
+                    \\  1 B| `world` Rc:2
+                , try debugStr(idc_if_it_leaks, old_root));
+
+                try eq(.{ 0, 3 }, .{ line, col });
+                try eqStr(
+                    \\3 2/14
+                    \\  2 1/9
+                    \\    1 B| `ok `
+                    \\    1 `hello` |E
+                    \\  1 B| `world` Rc:2
+                , try debugStr(idc_if_it_leaks, new_root));
+            }
 
             // freeing old_root first
             {
