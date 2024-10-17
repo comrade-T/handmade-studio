d7d63b9b08726b892727940aff28473d53fdd070
comrade-T <communism.app@gmail.com>
2024-10-17 16:36:11 +0700

    wip(UT): add untested UniformedInsertEventBuilder

--- src/buffer/UndoTree.zig
+++ src/buffer/UndoTree.zig
@@ -14,9 +14,74 @@ const eqStr = std.testing.expectEqualStrings;
 const eqSlice = std.testing.expectEqualSlices;
 const assert = std.debug.assert;
 
+const EventList = std.MultiArrayList(Event);
+
 //////////////////////////////////////////////////////////////////////////////////////////////
 
 a: Allocator,
+arena: ArenaAllocator,
+
+events: EventList,
+
+active_event_index: u16,
+
+fn init(a: Allocator) !UndoTree {
+    return UndoTree{
+        .a = a,
+        .arena = ArenaAllocator.init(a),
+    };
+}
+
+fn deinit(self: *@This()) void {
+    self.arena.deinit();
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////////
+
+fn finalizeUniformedInsertEvent(self: *@This(), node: RcNode, builder: UniformedInsertEventBuilder) !void {
+    const event = Event{
+        .node = node,
+        .parent = self.active_event_index,
+        .timestamp = std.time.milliTimestamp(),
+        .kind = .insert,
+        .children = .none,
+        .changes = Event.Changes{ .multiple_uniformed = .{
+            .ranges = self.arena.allocator().dupe(CursorRange, builder.ranges),
+            .contents = builder.contents,
+        } },
+    };
+    builder.deinit();
+    return event;
+}
+
+const UniformedInsertEventBuilder = struct {
+    a: Allocator,
+    ranges: ArrayList(CursorRange),
+    contents: []const u8 = "",
+
+    fn init(a: Allocator) !UniformedInsertEventBuilder {
+        return UniformedInsertEventBuilder{ .a = a, .ranges = ArrayList(CursorRange).init(a) };
+    }
+
+    fn deinit(self: *@This()) void {
+        self.ranges.deinit();
+        self.a.free(self.contents);
+    }
+
+    fn addInitialRange(self: *@This(), point: CursorPoint) !void {
+        try self.ranges.append(point);
+    }
+
+    fn updateRange(self: *@This(), i: usize, new_range: CursorRange) !void {
+        assert(i < self.ranges.items.len);
+        self.ranges.items[i] = new_range;
+    }
+
+    fn appendChars(self: *@This(), new_chars: []const u8) !void {
+        if (self.contents.len > 0) self.a.free(self.contents);
+        self.contents = try std.fmt.allocPrint(self.a, "{s}{s}", .{ self.contents, new_chars });
+    }
+};
 
 //////////////////////////////////////////////////////////////////////////////////////////////
 
@@ -49,8 +114,8 @@ const Event = struct {
     const Changes = union(enum) {
         none,
         single: Single,
-        multiple: []Single,
-        uniformed: Uniformed,
+        multiple_distinct: []Single,
+        multiple_uniformed: Uniformed,
 
         const Single = struct {
             range: CursorRange,
@@ -74,5 +139,5 @@ test {
     try eq(.{ 8, 24 }, .{ @alignOf(Event.Changes.Single), @sizeOf(Event.Changes.Single) });
     try eq(.{ 8, 32 }, .{ @alignOf(Event.Changes.Uniformed), @sizeOf(Event.Changes.Uniformed) });
     try eq(.{ 8, 40 }, .{ @alignOf(Event.Changes), @sizeOf(Event.Changes) });
-    try eq(.{ 8, 88 }, .{ @alignOf(Event), @sizeOf(Event) }); // 1000 events -> ~100kb
+    try eq(.{ 8, 88 }, .{ @alignOf(Event), @sizeOf(Event) }); // 1000 events -> ~100kb in memory
 }
