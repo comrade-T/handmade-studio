da756aa2a25184cdc60fd3624ce088758d4ff133
comrade-T <communism.app@gmail.com>
2024-11-04 19:36:41 +0700

    wip(cur): add getByteType()

--- src/window/CursorManager.zig
+++ src/window/CursorManager.zig
@@ -6,6 +6,8 @@ const testing_allocator = std.testing.allocator;
 const eq = std.testing.expectEqual;
 const assert = std.debug.assert;
 
+const code_point = @import("code_point");
+
 //////////////////////////////////////////////////////////////////////////////////////////////
 
 cursors: ArrayList(Cursor),
@@ -17,6 +19,8 @@ const GetLineCallback = *const fn (ctx: *anyopaque, a: Allocator, linenr: usize)
 const Cursor = struct {
     line: usize,
     col: usize,
+
+    ///////////////////////////// hjkl
 
     pub fn moveUp(self: *@This(), by: usize, noc_cb: GetNocInLineCallback, ctx: *anyopaque) void {
         self.line -|= by;
@@ -43,9 +47,29 @@ const Cursor = struct {
         const noc = noc_cb(ctx, self.line);
         if (self.col > noc) self.col = noc;
     }
+
+    ///////////////////////////// w/W
+
+    pub fn forwardWord(self: *@This(), a: Allocator, count: usize, line_cb: GetLineCallback, ctx: *anyopaque) void {
+        const line = line_cb(ctx, a, self.line);
+        defer a.free(line);
+
+        var iter = code_point.Iterator{ .bytes = line };
+        var i: usize = 0;
+        var current_col_byte_kind = ByteKind.not_found;
+        while (iter.next()) |cp| {
+            defer i += 1;
+            if (i == self.col) current_col_byte_kind = getByteType(u21, cp.code);
+        }
+
+        assert(current_col_byte_kind != .not_found);
+        std.debug.print("current_col_byte_kind: '{any}'\n", .{current_col_byte_kind});
+
+        _ = count;
+    }
 };
 
-test Cursor {
+test "Cursor - basic hjkl movements" {
     var ctx = TestingCtx{ .source = "hi\nworld\nhello\nx" };
     var c = Cursor{ .line = 0, .col = 0 };
 
@@ -97,10 +121,67 @@ test Cursor {
     }
 }
 
+////////////////////////////////////////////////////////////////////////////////////////////// Vim Movements
+
+test "Cursor - Vim w/W" {
+    var ctx = TestingCtx{ .source = "hello world\nhi venus" };
+    var c = Cursor{ .line = 0, .col = 0 };
+
+    {
+        c.forwardWord(testing_allocator, 1, TestingCtx.getLine, &ctx);
+        try eq(Cursor{ .line = 0, .col = 6 }, c);
+    }
+}
+
+const ByteKind = enum { spacing, symbol, maybe_char, not_found };
+
+fn getByteType(T: type, b: T) ByteKind {
+    return switch (b) {
+        ' ' => .spacing,
+        '\t' => .spacing,
+        '\n' => .spacing,
+
+        '=' => .symbol,
+        '"' => .symbol,
+        '\'' => .symbol,
+        '/' => .symbol,
+        '\\' => .symbol,
+        '*' => .symbol,
+        ':' => .symbol,
+        '.' => .symbol,
+        ',' => .symbol,
+        '(' => .symbol,
+        ')' => .symbol,
+        '{' => .symbol,
+        '}' => .symbol,
+        '[' => .symbol,
+        ']' => .symbol,
+        ';' => .symbol,
+        '|' => .symbol,
+        '?' => .symbol,
+        '&' => .symbol,
+        '#' => .symbol,
+        '-' => .symbol,
+
+        else => .maybe_char,
+    };
+}
+
 ////////////////////////////////////////////////////////////////////////////////////////////// Test Helpers
 
 const TestingCtx = struct {
     source: []const u8,
+
+    fn getLine(ctx: *anyopaque, a: Allocator, linenr: usize) []const u8 {
+        const self = @as(*@This(), @ptrCast(@alignCast(ctx)));
+        var split_iter = std.mem.split(u8, self.source, "\n");
+        var i: usize = 0;
+        while (split_iter.next()) |line| {
+            defer i += 1;
+            if (i == linenr) return a.dupe(u8, line) catch unreachable;
+        }
+        unreachable;
+    }
 
     fn getNumOfCharsInLine(ctx: *anyopaque, linenr: usize) usize {
         const self = @as(*@This(), @ptrCast(@alignCast(ctx)));
