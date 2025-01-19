41aab7edd2299036334c138627d3ed40d2b6b13b
comrade-T <communism.app@gmail.com>
2025-01-19 10:34:42 +0700

    ref: move raylib related callbacks to its own file

--- src/main.zig
+++ src/main.zig
@@ -18,6 +18,7 @@ const assert = std.debug.assert;
 
 const ztracy = @import("ztracy");
 const rl = @import("raylib");
+const rlcb = @import("raylib-related/raylib_callbacks.zig");
 
 const Smooth2DCamera = @import("raylib-related/Smooth2DCamera.zig");
 
@@ -38,7 +39,6 @@ const AnchorPicker = @import("AnchorPicker");
 
 const screen_width = 1920;
 const screen_height = 1080;
-const FONT_BASE_SIZE = 100;
 
 pub fn main() anyerror!void {
 
@@ -65,24 +65,24 @@ pub fn main() anyerror!void {
     ////////////////////////////////////////////////////////////////////////////////////////////// Interfacing w/ Raylib
 
     const info_callbacks = RenderMall.InfoCallbacks{
-        .getScreenWidthHeight = getScreenWidthHeight,
-        .getScreenToWorld2D = getScreenToWorld2D,
-        .getWorldToScreen2D = getWorldToScreen2D,
-        .getViewFromCamera = getViewFromCamera,
-        .cameraTargetsEqual = cameraTargetsEqual,
-        .getCameraZoom = getCameraZoom,
+        .getScreenWidthHeight = rlcb.getScreenWidthHeight,
+        .getScreenToWorld2D = rlcb.getScreenToWorld2D,
+        .getWorldToScreen2D = rlcb.getWorldToScreen2D,
+        .getViewFromCamera = rlcb.getViewFromCamera,
+        .cameraTargetsEqual = rlcb.cameraTargetsEqual,
+        .getCameraZoom = rlcb.getCameraZoom,
     };
 
     const render_callbacks = RenderMall.RenderCallbacks{
-        .drawCodePoint = drawCodePoint,
-        .drawRectangle = drawRectangle,
-        .drawRectangleLines = drawRectangleLines,
-        .drawCircle = drawCircle,
-        .drawLine = drawLine,
-        .changeCameraZoom = changeCameraZoom,
-        .changeCameraPan = changeCameraPan,
-        .beginScissorMode = beginScissorMode,
-        .endScissorMode = endScissorMode,
+        .drawCodePoint = rlcb.drawCodePoint,
+        .drawRectangle = rlcb.drawRectangle,
+        .drawRectangleLines = rlcb.drawRectangleLines,
+        .drawCircle = rlcb.drawCircle,
+        .drawLine = rlcb.drawLine,
+        .changeCameraZoom = rlcb.changeCameraZoom,
+        .changeCameraPan = rlcb.changeCameraPan,
+        .beginScissorMode = rlcb.beginScissorMode,
+        .endScissorMode = rlcb.endScissorMode,
     };
 
     ////////////////////////////////////////////////////////////////////////////////////////////// MappingCouncil
@@ -105,8 +105,8 @@ pub fn main() anyerror!void {
     var font_store = try FontStore.init(gpa);
     defer font_store.deinit();
 
-    var meslo = rl.loadFontEx("Meslo LG L DZ Regular Nerd Font Complete Mono.ttf", FONT_BASE_SIZE, null);
-    try addRaylibFontToFontStore(&meslo, "Meslo", &font_store);
+    var meslo = rl.loadFontEx("Meslo LG L DZ Regular Nerd Font Complete Mono.ttf", rlcb.FONT_BASE_SIZE, null);
+    try rlcb.addRaylibFontToFontStore(&meslo, "Meslo", &font_store);
 
     var colorscheme_store = try ColorschemeStore.init(gpa);
     defer colorscheme_store.deinit();
@@ -200,142 +200,5 @@ pub fn main() anyerror!void {
                 wm.render();
             }
         }
-    }
-}
-
-////////////////////////////////////////////////////////////////////////////////////////////// Render Callbacks //////////////////////////////////////////////////////////////////////////////////////////////
-
-fn drawCodePoint(font: *const FontStore.Font, code_point: u21, x: f32, y: f32, font_size: f32, color: u32) void {
-    assert(font.rl_font != null);
-    const rl_font = @as(*rl.Font, @ptrCast(@alignCast(font.rl_font)));
-    rl.drawTextCodepoint(rl_font.*, @intCast(code_point), .{ .x = x, .y = y }, font_size, rl.Color.fromInt(color));
-}
-
-fn drawRectangle(x: f32, y: f32, width: f32, height: f32, color: u32) void {
-    rl.drawRectangle(
-        @as(i32, @intFromFloat(x)),
-        @as(i32, @intFromFloat(y)),
-        @as(i32, @intFromFloat(width)),
-        @as(i32, @intFromFloat(height)),
-        rl.Color.fromInt(color),
-    );
-}
-
-fn drawRectangleLines(x: f32, y: f32, width: f32, height: f32, line_thick: f32, color: u32) void {
-    rl.drawRectangleLinesEx(
-        .{ .x = x, .y = y, .width = width, .height = height },
-        line_thick,
-        rl.Color.fromInt(color),
-    );
-}
-
-fn drawCircle(x: f32, y: f32, radius: f32, color: u32) void {
-    rl.drawCircleV(.{ .x = x, .y = y }, radius, rl.Color.fromInt(color));
-}
-
-fn drawLine(start_x: f32, start_y: f32, end_x: f32, end_y: f32, thickness: f32, color: u32) void {
-    rl.drawLineEx(
-        .{ .x = start_x, .y = start_y },
-        .{ .x = end_x, .y = end_y },
-        thickness,
-        rl.Color.fromInt(color),
-    );
-}
-
-fn changeCameraZoom(camera_: *anyopaque, target_camera_: *anyopaque, x: f32, y: f32, scale_factor: f32) void {
-    const camera = @as(*rl.Camera2D, @ptrCast(@alignCast(camera_)));
-    const target_camera = @as(*rl.Camera2D, @ptrCast(@alignCast(target_camera_)));
-
-    const anchor_world_pos = rl.getScreenToWorld2D(.{ .x = x, .y = y }, camera.*);
-
-    camera.offset = rl.Vector2{ .x = x, .y = y };
-    target_camera.offset = rl.Vector2{ .x = x, .y = y };
-
-    target_camera.target = anchor_world_pos;
-    camera.target = anchor_world_pos;
-
-    target_camera.zoom = rl.math.clamp(target_camera.zoom * scale_factor, 0.125, 64);
-}
-
-fn changeCameraPan(target_camera_: *anyopaque, x_by: f32, y_by: f32) void {
-    const target_camera = @as(*rl.Camera2D, @ptrCast(@alignCast(target_camera_)));
-    target_camera.*.target.x += x_by;
-    target_camera.*.target.y += y_by;
-}
-
-fn beginScissorMode(x: f32, y: f32, width: f32, height: f32) void {
-    rl.beginScissorMode(
-        @intFromFloat(x),
-        @intFromFloat(y),
-        @intFromFloat(width),
-        @intFromFloat(height),
-    );
-}
-
-fn endScissorMode() void {
-    rl.endScissorMode();
-}
-
-////////////////////////////////////////////////////////////////////////////////////////////// Info Callbacks //////////////////////////////////////////////////////////////////////////////////////////////
-
-fn getScreenWidthHeight() struct { f32, f32 } {
-    return .{
-        @as(f32, @floatFromInt(rl.getScreenWidth())),
-        @as(f32, @floatFromInt(rl.getScreenHeight())),
-    };
-}
-
-fn cameraTargetsEqual(a_: *anyopaque, b_: *anyopaque) bool {
-    const a = @as(*rl.Camera2D, @ptrCast(@alignCast(a_)));
-    const b = @as(*rl.Camera2D, @ptrCast(@alignCast(b_)));
-
-    return @round(a.target.x * 100) == @round(b.target.x * 100) and
-        @round(a.target.y * 100) == @round(b.target.y * 100);
-}
-
-fn getViewFromCamera(camera_: *anyopaque) RenderMall.ScreenView {
-    const camera = @as(*rl.Camera2D, @ptrCast(@alignCast(camera_)));
-    const start = rl.getScreenToWorld2D(.{ .x = 0, .y = 0 }, camera.*);
-    const end = rl.getScreenToWorld2D(.{
-        .x = @as(f32, @floatFromInt(rl.getScreenWidth())),
-        .y = @as(f32, @floatFromInt(rl.getScreenHeight())),
-    }, camera.*);
-
-    return RenderMall.ScreenView{
-        .start = .{ .x = start.x, .y = start.y },
-        .end = .{ .x = end.x, .y = end.y },
-    };
-}
-
-fn getScreenToWorld2D(camera_: *anyopaque, x: f32, y: f32) struct { f32, f32 } {
-    const camera = @as(*rl.Camera2D, @ptrCast(@alignCast(camera_)));
-    const result = rl.getScreenToWorld2D(.{ .x = x, .y = y }, camera.*);
-    return .{ result.x, result.y };
-}
-
-fn getWorldToScreen2D(camera_: *anyopaque, x: f32, y: f32) struct { f32, f32 } {
-    const camera = @as(*rl.Camera2D, @ptrCast(@alignCast(camera_)));
-    const result = rl.getWorldToScreen2D(.{ .x = x, .y = y }, camera.*);
-    return .{ result.x, result.y };
-}
-
-fn getCameraZoom(camera_: *anyopaque) f32 {
-    const camera = @as(*rl.Camera2D, @ptrCast(@alignCast(camera_)));
-    return camera.zoom;
-}
-
-////////////////////////////////////////////////////////////////////////////////////////////// Font //////////////////////////////////////////////////////////////////////////////////////////////
-
-fn addRaylibFontToFontStore(rl_font: *rl.Font, name: []const u8, store: *FontStore) !void {
-    rl.setTextureFilter(rl_font.texture, .texture_filter_trilinear);
-
-    try store.addNewFont(rl_font, name, FONT_BASE_SIZE, @floatFromInt(rl_font.ascent));
-    const f = store.map.getPtr(name) orelse unreachable;
-    for (0..@intCast(rl_font.glyphCount)) |i| {
-        try f.addGlyph(store.a, rl_font.glyphs[i].value, .{
-            .width = rl_font.recs[i].width,
-            .offsetX = @as(f32, @floatFromInt(rl_font.glyphs[i].offsetX)),
-            .advanceX = @as(f32, @floatFromInt(rl_font.glyphs[i].advanceX)),
-        });
     }
 }
---
+++ src/raylib-related/raylib_callbacks.zig
@@ -0,0 +1,144 @@
+const std = @import("std");
+const rl = @import("raylib");
+
+const FontStore = @import("FontStore");
+const RenderMall = @import("RenderMall");
+
+pub const FONT_BASE_SIZE = 100;
+
+////////////////////////////////////////////////////////////////////////////////////////////// Render Callbacks
+
+pub fn drawCodePoint(font: *const FontStore.Font, code_point: u21, x: f32, y: f32, font_size: f32, color: u32) void {
+    std.debug.assert(font.rl_font != null);
+    const rl_font = @as(*rl.Font, @ptrCast(@alignCast(font.rl_font)));
+    rl.drawTextCodepoint(rl_font.*, @intCast(code_point), .{ .x = x, .y = y }, font_size, rl.Color.fromInt(color));
+}
+
+pub fn drawRectangle(x: f32, y: f32, width: f32, height: f32, color: u32) void {
+    rl.drawRectangle(
+        @as(i32, @intFromFloat(x)),
+        @as(i32, @intFromFloat(y)),
+        @as(i32, @intFromFloat(width)),
+        @as(i32, @intFromFloat(height)),
+        rl.Color.fromInt(color),
+    );
+}
+
+pub fn drawRectangleLines(x: f32, y: f32, width: f32, height: f32, line_thick: f32, color: u32) void {
+    rl.drawRectangleLinesEx(
+        .{ .x = x, .y = y, .width = width, .height = height },
+        line_thick,
+        rl.Color.fromInt(color),
+    );
+}
+
+pub fn drawCircle(x: f32, y: f32, radius: f32, color: u32) void {
+    rl.drawCircleV(.{ .x = x, .y = y }, radius, rl.Color.fromInt(color));
+}
+
+pub fn drawLine(start_x: f32, start_y: f32, end_x: f32, end_y: f32, thickness: f32, color: u32) void {
+    rl.drawLineEx(
+        .{ .x = start_x, .y = start_y },
+        .{ .x = end_x, .y = end_y },
+        thickness,
+        rl.Color.fromInt(color),
+    );
+}
+
+pub fn changeCameraZoom(camera_: *anyopaque, target_camera_: *anyopaque, x: f32, y: f32, scale_factor: f32) void {
+    const camera = @as(*rl.Camera2D, @ptrCast(@alignCast(camera_)));
+    const target_camera = @as(*rl.Camera2D, @ptrCast(@alignCast(target_camera_)));
+
+    const anchor_world_pos = rl.getScreenToWorld2D(.{ .x = x, .y = y }, camera.*);
+
+    camera.offset = rl.Vector2{ .x = x, .y = y };
+    target_camera.offset = rl.Vector2{ .x = x, .y = y };
+
+    target_camera.target = anchor_world_pos;
+    camera.target = anchor_world_pos;
+
+    target_camera.zoom = rl.math.clamp(target_camera.zoom * scale_factor, 0.125, 64);
+}
+
+pub fn changeCameraPan(target_camera_: *anyopaque, x_by: f32, y_by: f32) void {
+    const target_camera = @as(*rl.Camera2D, @ptrCast(@alignCast(target_camera_)));
+    target_camera.*.target.x += x_by;
+    target_camera.*.target.y += y_by;
+}
+
+pub fn beginScissorMode(x: f32, y: f32, width: f32, height: f32) void {
+    rl.beginScissorMode(
+        @intFromFloat(x),
+        @intFromFloat(y),
+        @intFromFloat(width),
+        @intFromFloat(height),
+    );
+}
+
+pub fn endScissorMode() void {
+    rl.endScissorMode();
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////// Info Callbacks
+
+pub fn getScreenWidthHeight() struct { f32, f32 } {
+    return .{
+        @as(f32, @floatFromInt(rl.getScreenWidth())),
+        @as(f32, @floatFromInt(rl.getScreenHeight())),
+    };
+}
+
+pub fn cameraTargetsEqual(a_: *anyopaque, b_: *anyopaque) bool {
+    const a = @as(*rl.Camera2D, @ptrCast(@alignCast(a_)));
+    const b = @as(*rl.Camera2D, @ptrCast(@alignCast(b_)));
+
+    return @round(a.target.x * 100) == @round(b.target.x * 100) and
+        @round(a.target.y * 100) == @round(b.target.y * 100);
+}
+
+pub fn getViewFromCamera(camera_: *anyopaque) RenderMall.ScreenView {
+    const camera = @as(*rl.Camera2D, @ptrCast(@alignCast(camera_)));
+    const start = rl.getScreenToWorld2D(.{ .x = 0, .y = 0 }, camera.*);
+    const end = rl.getScreenToWorld2D(.{
+        .x = @as(f32, @floatFromInt(rl.getScreenWidth())),
+        .y = @as(f32, @floatFromInt(rl.getScreenHeight())),
+    }, camera.*);
+
+    return RenderMall.ScreenView{
+        .start = .{ .x = start.x, .y = start.y },
+        .end = .{ .x = end.x, .y = end.y },
+    };
+}
+
+pub fn getScreenToWorld2D(camera_: *anyopaque, x: f32, y: f32) struct { f32, f32 } {
+    const camera = @as(*rl.Camera2D, @ptrCast(@alignCast(camera_)));
+    const result = rl.getScreenToWorld2D(.{ .x = x, .y = y }, camera.*);
+    return .{ result.x, result.y };
+}
+
+pub fn getWorldToScreen2D(camera_: *anyopaque, x: f32, y: f32) struct { f32, f32 } {
+    const camera = @as(*rl.Camera2D, @ptrCast(@alignCast(camera_)));
+    const result = rl.getWorldToScreen2D(.{ .x = x, .y = y }, camera.*);
+    return .{ result.x, result.y };
+}
+
+pub fn getCameraZoom(camera_: *anyopaque) f32 {
+    const camera = @as(*rl.Camera2D, @ptrCast(@alignCast(camera_)));
+    return camera.zoom;
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////// Add Font
+
+pub fn addRaylibFontToFontStore(rl_font: *rl.Font, name: []const u8, store: *FontStore) !void {
+    rl.setTextureFilter(rl_font.texture, .texture_filter_trilinear);
+
+    try store.addNewFont(rl_font, name, FONT_BASE_SIZE, @floatFromInt(rl_font.ascent));
+    const f = store.map.getPtr(name) orelse unreachable;
+    for (0..@intCast(rl_font.glyphCount)) |i| {
+        try f.addGlyph(store.a, rl_font.glyphs[i].value, .{
+            .width = rl_font.recs[i].width,
+            .offsetX = @as(f32, @floatFromInt(rl_font.glyphs[i].offsetX)),
+            .advanceX = @as(f32, @floatFromInt(rl_font.glyphs[i].advanceX)),
+        });
+    }
+}
